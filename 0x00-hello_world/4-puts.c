5af4439047fc2cd84966aa76fc0fcf1e7ff84282
Dagmawii1 <Dagmawii1@github.com>
2022-04-07 15:46:37 -0700

    new 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - entry point
- *
- * Return: 0 after printing the function 
+ * main - print the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise 
  */
 int main(void)
 {
