59ff7f7e5bea5603f9b6d0f6fd9c71b4d6fc795c
Dagmawii1 <Dagmawii1@github.com>
2022-04-07 15:24:27 -0700

    new 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
-#include <studio.h>
-
+#include <stdio.h>
 /**
- *  main - Entry point
+ * main - entry point
  *
- *  Return: Always 0 (Success)
+ * Return: 0 after printing the function 
  */
 int main(void)
 {
-puts("\ "programing is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
 
