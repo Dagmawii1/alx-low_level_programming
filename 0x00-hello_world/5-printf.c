87fb66db8b0ff4aad4b51a1543a36d645f1134f5
Dagmawii1 <Dagmawii1@github.com>
2022-04-07 17:49:23 -0700

    new 4

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - main block
- * Return: 0
+ * main - Entry point
+ *
+ * Return: Always  (success)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,with proper grammar, but the outcome is a piece of art, \n");
+printf("with proper grammar, but the outcome is a piece of art, \n");
 return (0);
 }
